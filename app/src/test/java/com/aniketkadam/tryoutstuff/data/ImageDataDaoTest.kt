8f94ab0fefe9c0e3f897ba8f414a69e97043141d
Aniket Kadam <anikadamg@gmail.com>
2019-07-29 13:45:25 +0530

    Added robolectric,LiveData testing utils, and a test for the ImageDataDao
    It checks if data can be inserted and read back out.
    For additional detail on why it's done this way, check out
    https://op.aniketkadam.com/room-testing-off-device

---
+++ app/src/test/java/com/aniketkadam/tryoutstuff/data/ImageDataDaoTest.kt
@@ -0,0 +1,48 @@
+package com.aniketkadam.tryoutstuff.data
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.filters.LargeTest
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.io.IOException
+
+@LargeTest
+@RunWith(RobolectricTestRunner::class)
+class ImageDataDaoTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private lateinit var imageDataDao: ImageDataDao
+    private lateinit var db: ImageDatabase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, ImageDatabase::class.java).allowMainThreadQueries().build()
+        imageDataDao = db.imageDataDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() = db.close()
+
+    @Test
+    fun writeImageAndRead() {
+        val sampleData = ImageData("what", "Hello", "www.imagedb.com")
+
+        imageDataDao.insert(sampleData).blockingGet()
+
+        val result: List<ImageData>? = LiveDataTestUtil.getValue(imageDataDao.getAllImageData())
+        assertEquals(result?.get(0), sampleData)
+
+    }
+}
