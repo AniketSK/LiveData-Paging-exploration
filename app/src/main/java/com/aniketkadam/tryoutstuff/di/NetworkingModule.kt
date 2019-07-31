886a6f5c7fc24db39e721907d1f36ebe79892a3f
Aniket Kadam <anikadamg@gmail.com>
2019-07-31 16:49:10 +0530

    Add low level logging to the http interceptor.

--- app/src/main/java/com/aniketkadam/tryoutstuff/di/NetworkingModule.kt
+++ app/src/main/java/com/aniketkadam/tryoutstuff/di/NetworkingModule.kt
@@ -3,6 +3,7 @@ package com.aniketkadam.tryoutstuff.di
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -20,5 +21,6 @@ class NetworkingModule {
 
     @Singleton
     @Provides
-    fun providesOkHttpClient() = OkHttpClient()
+    fun providesOkHttpClient() = OkHttpClient.Builder().addNetworkInterceptor((HttpLoggingInterceptor())
+        .apply { level = HttpLoggingInterceptor.Level.BODY }).build()
 }
