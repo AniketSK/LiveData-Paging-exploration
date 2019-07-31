4ea91e9e09027f7d320e63f2d751a41aed9deb5b
Aniket Kadam <anikadamg@gmail.com>
2019-08-01 00:13:04 +0530

    Moved the url that the images api uses to a constant
    Mainly so that RESTMockServer can correctly identify which response to
    send.

--- app/src/main/java/com/aniketkadam/tryoutstuff/network/ImageApi.kt
+++ app/src/main/java/com/aniketkadam/tryoutstuff/network/ImageApi.kt
@@ -4,9 +4,9 @@ import com.aniketkadam.tryoutstuff.data.ImageData
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
-
+const val IMAGES_LOCATION = "someUrl"
 interface ImageApi {
 
-    @GET("someUrl")
+    @GET(IMAGES_LOCATION)
     fun findImages(@Query("id") id: String): Single<List<ImageData>>
 }
