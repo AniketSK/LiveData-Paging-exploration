05fb8b986256768f29cb89f643855f026e90411e
Aniket Kadam <anikadamg@gmail.com>
2019-08-01 00:10:23 +0530

    Added a test for a mocked element showing up on the list in the recyclerview
    The 'Shuri' element is passed in via the mocked network call.
    It is expected to show up at some point and this, currently failing
    test, looks for it.

---
+++ app/src/androidTest/java/com/aniketkadam/tryoutstuff/MainActivityViewLoadingTest.kt
@@ -0,0 +1,24 @@
+package com.aniketkadam.tryoutstuff
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityViewLoadingTest {
+
+    @get:Rule
+    val activityTestRule = ActivityTestRule(MainActivity::class.java, false, false)
+
+    @Test
+    fun checkIfLaunches() {
+        activityTestRule.launchActivity(null)
+        onView(withText("Shuri")).check(matches(isDisplayed()))
+    }
+}
