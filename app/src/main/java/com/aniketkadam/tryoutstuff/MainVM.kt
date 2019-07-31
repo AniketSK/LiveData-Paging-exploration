6a618a80195cd0f04161dd6ddf7f0e9d6e92cbfc
Aniket Kadam <anikadamg@gmail.com>
2019-07-31 16:50:03 +0530

    Exposing the image list and the network state from the VM

--- app/src/main/java/com/aniketkadam/tryoutstuff/MainVM.kt
+++ app/src/main/java/com/aniketkadam/tryoutstuff/MainVM.kt
@@ -1,9 +1,17 @@
 package com.aniketkadam.tryoutstuff
 
 import androidx.lifecycle.ViewModel
+import com.aniketkadam.tryoutstuff.data.ImageListResult
 import com.aniketkadam.tryoutstuff.data.Repository
 import javax.inject.Inject
 
 class MainVM @Inject constructor(val respository: Repository) : ViewModel() {
+
+    private val _imageListResult: ImageListResult by lazy {
+        respository.getPagedImageResult()
+    }
+
+    val imageList = _imageListResult.imageData
+    val networkState = _imageListResult.networkState
 
 }
