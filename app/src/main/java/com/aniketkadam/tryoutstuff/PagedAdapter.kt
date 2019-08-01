31cde5e0fc964a06018166a7a24b2506ffab17f6
Aniket Kadam <anikadamg@gmail.com>
2019-08-02 00:06:16 +0530

    Implemented the PagedAdapter for imagedata

---
+++ app/src/main/java/com/aniketkadam/tryoutstuff/PagedAdapter.kt
@@ -0,0 +1,40 @@
+package com.aniketkadam.tryoutstuff
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import com.aniketkadam.tryoutstuff.data.ImageData
+import com.aniketkadam.tryoutstuff.databinding.DataItemBinding
+
+class PagedAdapter : PagedListAdapter<ImageData, ImageDataViewHolder>(REPO_COMPARATOR) {
+
+    private var layoutInflater: LayoutInflater? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageDataViewHolder {
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.context)
+        }
+        // No way this layoutInflater can be null, also better crash early if it somehow is
+        DataBindingUtil.inflate<DataItemBinding>(layoutInflater!!, R.layout.data_item, parent, false).let {
+            return ImageDataViewHolder(it)
+        }
+    }
+
+    // Crash it early if the item is somehow null
+    override fun onBindViewHolder(holder: ImageDataViewHolder, position: Int) = holder.bind(getItem(position)!!)
+
+    companion object {
+        private val REPO_COMPARATOR = object : DiffUtil.ItemCallback<ImageData>() {
+
+            override fun areContentsTheSame(oldItem: ImageData, newItem: ImageData): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areItemsTheSame(old: ImageData, new: ImageData): Boolean =
+                old == new
+
+
+        }
+    }
+}
