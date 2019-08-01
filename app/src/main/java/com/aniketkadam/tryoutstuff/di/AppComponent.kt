2e6d1990f78442d23bbe7e82181a31d4f653b5e9
Aniket Kadam <anikadamg@gmail.com>
2019-08-01 14:10:10 +0530

    Added comments explaining where the AppComponent is modified so that androidTest runs can happen.

--- app/src/main/java/com/aniketkadam/tryoutstuff/di/AppComponent.kt
+++ app/src/main/java/com/aniketkadam/tryoutstuff/di/AppComponent.kt
@@ -9,16 +9,34 @@ import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AndroidSupportInjectionModule::class, AppModule::class, ActivityBuilder::class, DatabaseModule::class, NetworkingModule::class])
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class,
+        AppModule::class,
+        ActivityBuilder::class,
+        DatabaseModule::class,
+        NetworkingModule::class] // Any non-abstract class addition here requires reading the comment in the Builder
+)
 interface AppComponent : AndroidInjector<TryOutStuffApplication> {
     override fun inject(instance: TryOutStuffApplication)
 
     @Component.Builder
     interface Builder {
 
-        fun databaseModule(databaseModule: DatabaseModule) : Builder
-        fun networkingModule(networkingModule: NetworkingModule) : Builder
+        /**
+         * Methods for Instrumentation Testing
+         * Any non abstract class that's being passed a module to the AppComponent, needs to have a builder
+         * defined here so that if you want to mock it with DaggerMock,
+         * (note the test rule for it in EspressoDaggerMockRule )
+         * Then daggermock can inject the modules here.
+         * If new modules are added to this class, they must have corresponding builders so androidTest
+         * can continue to work.
+         */
+        fun databaseModule(databaseModule: DatabaseModule): Builder
 
+        fun networkingModule(networkingModule: NetworkingModule): Builder
+
+        // Standard dagger-android methods from here on
         @BindsInstance
         fun application(application: Application): Builder
 
