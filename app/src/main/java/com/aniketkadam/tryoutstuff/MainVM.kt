16899bce5caa96bb4a1085bdeb06f9fc58b244e5
Aniket Kadam <anikadamg@gmail.com>
2019-08-02 12:22:09 +0530

    We'll be using a fragment to navigate to the opened view so let's move both the list and the selected item view into fragments. We'll navigate between them by whatever the VM considers active.

--- app/src/main/java/com/aniketkadam/tryoutstuff/MainVM.kt
+++ app/src/main/java/com/aniketkadam/tryoutstuff/MainVM.kt
@@ -1,6 +1,9 @@
 package com.aniketkadam.tryoutstuff
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.aniketkadam.tryoutstuff.data.ImageData
 import com.aniketkadam.tryoutstuff.data.Repository
 import javax.inject.Inject
 
@@ -13,4 +16,15 @@ class MainVM @Inject constructor(private val repository: Repository) : ViewModel
     val imageList = _imageListResult.imageData
     val networkState = _imageListResult.networkState
 
+    val selectedItem = MutableLiveData<ImageData>()
+
+    // When an item is selected, launch the fragment that is supposed to show it.
+    private val _navigate = MutableLiveData<ActiveFragment>().apply { value = ActiveFragment.List }
+    val navigate: LiveData<ActiveFragment>
+        get() = _navigate
+}
+
+sealed class ActiveFragment {
+    object List : ActiveFragment()
+    object Selection : ActiveFragment()
 }
