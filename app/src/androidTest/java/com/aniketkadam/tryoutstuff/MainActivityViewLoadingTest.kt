eb9053ca75d5ab533a655a960228e5e4e36ac1f3
Aniket Kadam <anikadamg@gmail.com>
2019-08-03 16:18:26 +0530

    Added a test to make sure the detail view loads

--- app/src/androidTest/java/com/aniketkadam/tryoutstuff/MainActivityViewLoadingTest.kt
+++ app/src/androidTest/java/com/aniketkadam/tryoutstuff/MainActivityViewLoadingTest.kt
@@ -1,16 +1,14 @@
 package com.aniketkadam.tryoutstuff
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.Thread.sleep
-import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityViewLoadingTest {
@@ -22,7 +20,13 @@ class MainActivityViewLoadingTest {
     fun checkIfLaunches() {
         activityTestRule.launchActivity(null)
         onView(withText("Shuri")).check(matches(isDisplayed()))
-        sleep(TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS))
+    }
+
+    @Test
+    fun clicking_on_an_element_loads_the_detail_view() {
+        activityTestRule.launchActivity(null)
+        onView(withText("Shuri")).perform(click())
+        onView(withContentDescription("Shuri")).check(matches(isDisplayed()))
     }
 
 }
