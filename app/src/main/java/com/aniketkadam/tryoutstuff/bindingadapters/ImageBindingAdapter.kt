42d589c785aee24ff07545e5ef539793919189a0
Aniket Kadam <anikadamg@gmail.com>
2019-08-03 15:34:24 +0530

    Added a placeholder loading drawable and a random error drawable

--- app/src/main/java/com/aniketkadam/tryoutstuff/bindingadapters/ImageBindingAdapter.kt
+++ app/src/main/java/com/aniketkadam/tryoutstuff/bindingadapters/ImageBindingAdapter.kt
@@ -2,6 +2,7 @@ package com.aniketkadam.tryoutstuff.bindingadapters
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 
 object ImageBindingAdapter {
@@ -11,6 +12,12 @@ object ImageBindingAdapter {
     fun loadImage(view: ImageView, imageUrl: String) {
         Glide.with(view.context)
             .load(imageUrl).fitCenter()
+            .error(android.R.drawable.ic_menu_upload_you_tube)
+            .placeholder(CircularProgressDrawable(view.context).apply {
+                strokeWidth = 5f
+                centerRadius = 30f
+                start()
+            })
             .into(view)
     }
 }
