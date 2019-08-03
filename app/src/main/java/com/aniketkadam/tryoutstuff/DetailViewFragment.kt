a449bc330bff3e57ba4d4df3b5bab6eebe67b3e0
Aniket Kadam <anikadamg@gmail.com>
2019-08-03 15:18:45 +0530

    Added the fragment vm to detail view so it loads the correct fragment. Also changed the way the image data is bound so it loads immediately

--- app/src/main/java/com/aniketkadam/tryoutstuff/DetailViewFragment.kt
+++ app/src/main/java/com/aniketkadam/tryoutstuff/DetailViewFragment.kt
@@ -6,16 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.aniketkadam.tryoutstuff.databinding.DetailFragmentBinding
+import com.aniketkadam.tryoutstuff.di.FRAGMENT_VM
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
+import javax.inject.Named
 
 class DetailViewFragment : DaggerFragment() {
+
     @Inject
+    @field:Named(FRAGMENT_VM)
     lateinit var mainVM: MainVM
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        DataBindingUtil.inflate<DetailFragmentBinding>(inflater, R.layout.detail_fragment, container, false)
-            .apply {
-                setVariable(BR.imageData, mainVM.selectedItem.value)
-            }.root
+        DataBindingUtil.inflate<DetailFragmentBinding>(inflater, R.layout.detail_fragment, container, false).apply {
+            imageData = mainVM.selectedItem.value
+        }.root
+
 }
