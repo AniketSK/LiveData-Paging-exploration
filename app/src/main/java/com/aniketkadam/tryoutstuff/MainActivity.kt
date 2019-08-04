3263e8b54ecb10fcdc51164d6e12cd4940449899
Aniket Kadam <anikadamg@gmail.com>
2019-08-04 15:46:09 +0530

    Changed how the navigation works. Since we only need item positions, keep track of which position item and which fragment it should be loaded on. Everything else is handled by the recyclerview.

--- app/src/main/java/com/aniketkadam/tryoutstuff/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/tryoutstuff/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import com.aniketkadam.tryoutstuff.di.ACTIVITY_VM
 import dagger.android.support.DaggerAppCompatActivity
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -18,14 +19,24 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainVM.navigate.observe(this, Observer { navigate(it) })
+        mainVM.itemToNavigate.observe(this, Observer { navigate(it) })
     }
 
-    private fun navigate(it: ActiveFragment?) {
-        when (it) {
-            is ActiveFragment.Selection -> {
-                navTo(ListFragmentDirections.actionListFragmentToDetailViewFragment())
-            }
+    private fun navigate(it: PositionOnFragment?) {
+
+        if (it == null || it.position == -1) return // If it's null or represents default args, ignore navigation.
+
+        when (it.af) {
+            is ActiveFragment.Selection -> navTo(
+                ListFragmentDirections.actionListFragmentToDetailViewFragment().setResumeItemPosition(
+                    it.position
+                )
+            )
+            is ActiveFragment.List -> navTo(
+                DetailViewFragmentDirections.actionDetailViewFragmentToListFragment().setResumeItemPosition(
+                    it.position
+                )
+            )
         }
     }
 
--- app/src/main/java/com/aniketkadam/tryoutstuff/MainVM.kt
+++ app/src/main/java/com/aniketkadam/tryoutstuff/MainVM.kt
@@ -2,9 +2,7 @@ package com.aniketkadam.tryoutstuff
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import com.aniketkadam.tryoutstuff.data.ImageData
 import com.aniketkadam.tryoutstuff.data.Repository
 import javax.inject.Inject
 
@@ -18,14 +16,20 @@ class MainVM @Inject constructor(private val repository: Repository) : ViewModel
     val networkState = _imageListResult.networkState // This should only be shown as the last item in the recyclerview. So it's only visible when scrolled to bottom.
                                                                             // needs a different way to show loading when the expanded view is open though.
 
-    val selectedItem = MutableLiveData<ImageData?>().apply { value = null }
+    private val _itemToNavigate = MutableLiveData<PositionOnFragment?>().apply { value = null }
 
-    // When an item is selected, launch the fragment that is supposed to show it.
-    val navigate: LiveData<ActiveFragment> =
-        Transformations.map(selectedItem) { if (it == null) ActiveFragment.List else ActiveFragment.Selection }
+    val itemToNavigate: LiveData<PositionOnFragment?>
+        get() = _itemToNavigate
+
+    fun setItemToNavigate(pf: PositionOnFragment) {
+        _itemToNavigate.value = pf
+    }
 }
 
 sealed class ActiveFragment {
     object List : ActiveFragment()
     object Selection : ActiveFragment()
 }
+
+// Which fragment to load, and what position to load on it.
+data class PositionOnFragment(val af: ActiveFragment, val position: Int)
